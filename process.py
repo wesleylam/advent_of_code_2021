cb464b9ff119e4b0e7699835055c6302d812748f
wesley.lam <wesley.lam@conserveitiot.com>
2021-12-04 13:12:42 +1100

    Day1 task2

--- process.py
+++ process.py
@@ -1,19 +1,33 @@
-inputFile = 'input.txt'
-
+inputFile = 'source/day1.txt'
 with open(inputFile) as f:
-  incCount = -1
+  slidingWindow = []
+  incCount = 0
   decCount = 0
-  prev = -1
-  for line in f.readlines():
+  eqCount = 0
+  for i, line in enumerate(f.readlines()):
     num = int(line[:-1])
-    print(num)
-    if num > prev:
-      incCount += 1
-    elif num < prev:
-      decCount += 1
-    else:
-      raise Exception("Should this happen?")
-    prev = num
     
+    if i == 2: # set the first sum
+      prev = sum(slidingWindow)
+    
+    if i >= 3: # start comparing there are 2 groups with 3 elements
+      # update sliding window and calculate
+      slidingWindow.pop(0)
+      slidingWindow.append(num)
+      currentSum = sum(slidingWindow)
+      
+      # Compare
+      if currentSum > prev:
+        incCount += 1
+      elif currentSum < prev:
+        decCount += 1
+      else:
+        eqCount += 1
+    
+      prev = currentSum
+    else:
+      slidingWindow.append(num)
+
 print("Increased:", incCount)
 print("Decreased:", decCount)
+print("No Change:", eqCount)
